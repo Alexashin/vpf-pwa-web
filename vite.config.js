2b655a4bfd36c3b43c182f0ff34d2da66bfaf7b9
Alexashin Artem <aleksashin.ar@gmail.com>
2025-06-12 23:49:13 +0300

    deploy fixes

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,21 @@
-// Конфигурация Vite с поддержкой мультистраничного режима.
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+    base: '/vpf-pwa-web/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about.html'),
+                schedule: resolve(__dirname, 'schedule.html'),
+                partners: resolve(__dirname, 'partners.html'),
+                program: resolve(__dirname, 'program.html'),
+                map: resolve(__dirname, 'map.html'),
+                location: resolve(__dirname, 'location.html'),
+                contacts: resolve(__dirname, 'contacts.html'),
+                help: resolve(__dirname, 'help.html'),
+            }
+        }
+    }
+})
